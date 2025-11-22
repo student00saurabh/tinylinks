b328f46ed08ed438570bcbf6423ea2e9b0748fb2
Manish Kumar Mishra <manishmishra1898@gmail.com>
2025-11-23 00:30:30 +0530

    new

--- app.js
+++ app.js
@@ -61,14 +61,16 @@ const sessionOptions = {
   store,
   secret: process.env.SECRET,
   resave: false,
-  saveUninitialized: true,
+  saveUninitialized: false,
   cookie: {
-    expire: Date.now() + 7 * 24 * 60 * 60 * 1000,
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
   },
 };
+
+app.set("trust proxy", 1);
 
 app.use(session(sessionOptions));
 app.use(flash());
